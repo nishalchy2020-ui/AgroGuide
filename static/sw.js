555d321649a2e6fdbebc9c954a06554dd1736da4
nishalchy2020-ui <nishalchy2020@gmail.com>
2026-04-22 05:03:34 +0100

    Add PWA support with manifest, service worker, and icons

---
+++ static/sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = "agroguide-v1";
+const urlsToCache = [
+  "/",
+  "/static/css/style.css",
+  "/static/js/script.js",
+  "/static/manifest.json"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
